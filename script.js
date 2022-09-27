b7e02d40543c8f608468d4f4d4d0a2dcce233fb9
irakli-dadu <lipsowow@gmail.com>
2022-09-27 15:22:19 +0400

    4:48:00
    ...

---
+++ script.js
@@ -0,0 +1,78 @@
+// let personObject = {
+//     name: "irakli",
+//     age: 29,
+//     country: "Georgia"
+// }
+
+// function logData() {
+//     let personInfo = personObject.name + " is " + personObject.age + " years old and lives in " + personObject.country;
+
+//     console.log(personInfo);
+// }
+
+// logData()
+
+
+
+// let age = 18;
+
+// if(age < 6) {
+//     console.log("free");
+// }else if (age >= 6 && age <= 17) {
+//     console.log("child discount");
+// }else if(age >= 18 && age <= 26) {
+//     console.log("student discount");
+// }else if (age >= 27 && age <= 66) {
+//     console.log("full price");
+// }else {
+//     console.log("senior citizen discount");
+// }
+
+
+
+// let largeCountries = ["CHINA", "INDIA", "USA", "INDONESIA", "PAKISTAN"];
+// console.log("5 biggest countries in the world:");
+// for ( let i = 0; i < largeCountries.length; i++) {
+//     console.log("- " + largeCountries[i])
+// }
+
+
+
+
+// let largeCountries = ["TUVALU", "INDIA", "USA", "INDONESIA", "MONACO"];
+
+// largeCountries.shift();
+
+// largeCountries.unshift("CHINA");
+
+// largeCountries.pop();
+
+// largeCountries.push("PAKISTAN");
+
+// console.log(largeCountries);
+
+
+
+// let dayOfmonth = 13;
+// let weekDay = "FRIDAY";
+
+// if (dayOfmonth && weekDay) {
+//     console.log("SPOOKIEFACE");
+// }
+
+
+
+// let hands = ["rock", "paper", "scissor"];
+
+// function randomItem() {
+//     let randomIndex = Math.floor(Math.random() * 3);
+//     return hands[randomIndex]
+// }
+
+// console.log(randomItem());
+
+
+
+
+
+
